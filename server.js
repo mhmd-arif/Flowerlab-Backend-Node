6dc68f42875e8c17621c4bcf13b23355e49f1c67
usernamemu <mhmdarif.developer@gmail.com>
2023-05-29 13:03:54 +0700

    feat(auth): login register

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const flowerRoutes = require('./routes/flowerRoute')
+const userRoutes = require('./routes/user')
 
 const app = express()
 
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/flowers', flowerRoutes)
+app.use('/api/user', userRoutes)
 
 // connect db
 mongoose.connect(process.env.MONGO_URI)
